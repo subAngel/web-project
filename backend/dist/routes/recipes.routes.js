201e988d14d92f692c57eaeda122493197a5f0ba
subAngel <whoangel.agl@gmail.com>
2022-11-05 11:06:50 -0600

    Creando otra app de vite

--- backend/dist/routes/recipes.routes.js
+++ backend/dist/routes/recipes.routes.js
@@ -7,6 +7,7 @@ router.get("/:user/your-recipes", recipes_controller_1.getRecipes);
 router.post("/:user/your-recipes", recipes_controller_1.createRecipe);
 router.put("/:user/your-recipes/:id", recipes_controller_1.updateRecipe);
 router.delete("/:user/your-recipes/:id", recipes_controller_1.deleteRecipe);
+// TODO arreglar este get
 router.get("/:user/your-recipes/:id", recipes_controller_1.getRecipe);
 router.get("/all-recipes", recipes_controller_1.getAllRecipes);
 exports.default = router;
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,16 @@
-import express, { urlencoded } from "express";
+import express, { Request, urlencoded } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes";
 import recipesRoutes from "./routes/recipes.routes";
+import { format } from "timeago.js";
+import multer, { FileFilterCallback } from "multer";
+import { uuid } from "uuidv4";
+import * as path from "path";
+
+// * types
+type DestinationCallback = (error: Error | null, destination: string) => void;
+type FileNameCallback = (error: Error | null, filename: string) => void;
 
 // initializations
 const app = express();
@@ -15,11 +23,25 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
+const storage = multer.diskStorage({
+	destination: path.join(__dirname, "public/img/uploads"),
+	filename: (
+		req: Request,
+		file: Express.Multer.File,
+		callback: DestinationCallback
+	): void => {
+		callback(null, uuid() + path.extname(file.originalname));
+	},
+});
+app.use(multer({ storage: storage }).single("image"));
 // routes
 app.get("/", (req, res) => {
 	res.json({ msg: `The api is http://localhost:${app.get("port")}` });
 });
+
+// * static files
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(authRoutes);
 app.use(recipesRoutes);
 export default app;
