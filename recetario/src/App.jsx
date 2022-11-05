efd506857b1a7469bade7f760aeb685e4b4c507c
subAngel <whoangel.agl@gmail.com>
2022-11-05 12:20:22 -0600

    RecetarioLogin: Login con tailwind y daisyui

--- recetario/src/App.jsx
+++ recetario/src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/Login";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+			<div className="max-w-md w-full space-y-8">
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<LoginPage />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ recetario/src/constants/formFields.js
@@ -0,0 +1,22 @@
+const loginFields = [
+	{
+		labelText: "Nombre de usuario",
+		labelFor: "username",
+		id: "username",
+		name: "username",
+		type: "text",
+		isRequired: true,
+		placeholder: "Nombre de usuario",
+	},
+	{
+		labelText: "Contraseña",
+		labelFor: "password",
+		id: "password",
+		name: "password",
+		type: "password",
+		isRequired: true,
+		placeholder: "Contraseña",
+	},
+];
+
+export { loginFields };
