07b74755115eec975b9820021defd8182f5e2c43
subAngel <whoangel.agl@gmail.com>
2022-11-06 20:34:52 -0600

    Frontend: linkeando la imagen a home

--- recetario/src/components/Header.jsx
+++ recetario/src/components/Header.jsx
@@ -4,11 +4,13 @@ export default function Header({ heading, paragraph, linkName, linkUrl = "#" }) 
 	return (
 		<div className="mb-10">
 			<div className="flex justify-center">
-				<img
-					src="https://i.pinimg.com/originals/95/f3/4e/95f34e9bc588226a8c91a1be5bbebd67.png"
-					alt="Imagen del recetario"
-					className="h-24 w-24"
-				/>
+				<Link>
+					<img
+						src="https://i.pinimg.com/originals/95/f3/4e/95f34e9bc588226a8c91a1be5bbebd67.png"
+						alt="Imagen del recetario"
+						className="h-24 w-24"
+					/>
+				</Link>
 			</div>
 			<h2 className="mt-6 text-center text-3xl font-extrabold">{heading}</h2>
 			<p className="text-center text-sm mt-5">
