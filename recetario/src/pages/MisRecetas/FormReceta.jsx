edf8e945e5429a847196ea8515bc7a5a605ff3d3
subAngel <whoangel.agl@gmail.com>
2022-11-13 20:47:50 -0600

    Form recetas

--- recetario/src/pages/MisRecetas/FormReceta.jsx
+++ recetario/src/pages/MisRecetas/FormReceta.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IoMdImages } from "react-icons/io";
 
 function FormReceta() {
 	return (
@@ -9,14 +10,33 @@ function FormReceta() {
 					<Link to="/mis-recetas">Inicio</Link>
 				</button>
 				{/* <h2 className="text-center text-5xl font-bold mt-10 ">Formulario</h2> */}
-				<div className="flex flex-col sm:justify-center items-center mt-10">
-					<form className="w-full max-w-5xl">
-						<input
-							type="text"
-							id="large-input"
-							class="p-4 w-full rounded-lg text-3xl"
-							placeholder="Nombre de la receta Ej. pollo asado"
-						/>
+				<div className="flex flex-col sm:justify-center items-center mt-10 ">
+					<form className="w-full max-w-5xl md:flex md:items-center mb-6 bg-yellow-100 rounded-lg">
+						<div className="form-control">
+							<div className="md:w-5/6">
+								<input
+									type="text"
+									id="large-input"
+									className="p-4 w-full rounded-lg text-3xl m-8"
+									placeholder="Nombre de la receta Ej. pollo asado"
+								/>
+							</div>
+							<div className="md:w-1/6">
+								<p className="btn btn-secondary">
+									<IoMdImages className="text-4xl" />
+								</p>
+							</div>
+						</div>
+						<div className="form-control">
+							<div className="w-full">
+								<textarea
+									name=""
+									id=""
+									placeholder="Descripcion del platillo"
+									className="textarea w-full max-w-lg"
+								></textarea>
+							</div>
+						</div>
 					</form>
 				</div>
 			</div>
