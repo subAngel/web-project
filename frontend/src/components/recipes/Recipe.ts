72c37cb59bba143b8b062985d5a66c2c2e66955a
Ángel Jesús Zorrilla Cuevas <81493134+subAngel@users.noreply.github.com>
2022-11-23 17:06:12 -0600

    Delete frontend directory

--- frontend/src/components/recipes/Recipe.ts
+++
@@ -1,4 +0,0 @@
-export interface Recipe {
-	recipe_name: string;
-	user: string;
-}
--- frontend/src/components/recipes/recipeService.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const getRecipes = async () => {
-	return await axios.get("http://localhost:4000/all-recipes");
-};
